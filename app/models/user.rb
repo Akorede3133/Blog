57351b4b07ef6573b6e12ab5d629535f51d60941
akorede <emiolasaheed1@gmail.com>
2023-10-23 18:34:50 +0100

    Set up associations between models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :comments
+  has_many :likes
+  has_many :posts, foreign_key: 'author_id'
+  has_many :posts, through: :likes, foreign_key: 'author-id'
 end
