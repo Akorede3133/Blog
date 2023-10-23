0f34fb1a706d82b141ed0df2fc011622cf39b78d
akorede <emiolasaheed1@gmail.com>
2023-10-23 16:31:12 +0100

    Add user ref to likes table

--- db/migrate/20231023144844_add_author_ref_to_posts.rb
+++ db/migrate/20231023144844_add_author_ref_to_posts.rb
@@ -1,7 +1,6 @@
 class AddAuthorRefToPosts < ActiveRecord::Migration[7.1]
   def change
-    add_column :posts, :author_id, :integer
-    add_reference :posts, :author_id, null: false, foreign_key: true
+    add_foreign_key :posts, :users, column: :author_id
     add_index :posts, :author_id
   end
 end
