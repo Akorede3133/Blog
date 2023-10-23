93284e820d2be2a9f5d09b81486e6abe17b0628b
akorede <emiolasaheed1@gmail.com>
2023-10-23 16:35:32 +0100

    Add authot_id column to posts

--- db/migrate/20231023144844_add_author_ref_to_posts.rb
+++ db/migrate/20231023144844_add_author_ref_to_posts.rb
@@ -1,5 +1,6 @@
 class AddAuthorRefToPosts < ActiveRecord::Migration[7.1]
   def change
+    add_column :posts :author_id, :string
     add_foreign_key :posts, :users, column: :author_id
     add_index :posts, :author_id
   end
