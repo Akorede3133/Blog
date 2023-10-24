bbf7a1257ab82a11a41f2778dfb1a287297dc589
akorede <emiolasaheed1@gmail.com>
2023-10-24 10:15:30 +0100

    Write test for Post to not be valid when comments_counter is not an integer

--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  user = User.new(name: 'Jerry', photo: 'jerry.png', bio: 'This is Jerry bio', posts_counter: 0)
+  let(:user) { User.new(name: 'Jerry', photo: 'jerry.png', bio: 'This is Jerry bio', posts_counter: 0) }
   subject { Post.new(author: user, title: 'My first post', text: 'Content of my post', comments_counter: 0, likes_counter: 0) }
 
   before { subject.save }
-
   it 'is valid with the required attributes' do
     expect(subject).to be_valid
+  end
+
+  it 'is not valid when comments_counter is not an integer' do
+    subject.comments_counter = 'a'
+    expect(subject).not_to be_valid
   end
 end
