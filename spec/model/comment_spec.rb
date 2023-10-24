671e4635147d0862da2cb504d71dd541d77f767e
akorede <emiolasaheed1@gmail.com>
2023-10-24 11:05:06 +0100

    Write test for comment model to test for its validity with require attributes

---
+++ spec/model/comment_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  let(:user) { User.new(name: 'Jerry', photo: 'jerry.png', bio: 'This is Jerry bio', posts_counter: 0) }
+  let(:post) { Post.new(author: user, title: 'My first post', text: 'Content of my post', comments_counter: 0, likes_counter: 0) }
+  subject do
+    Comment.new(user: user, post: post, text: 'This is the comment')
+  end
+
+  it 'is valid with the required attributes' do
+    expect(subject).to be_valid
+  end
+end
