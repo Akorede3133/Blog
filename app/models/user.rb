36aeb48d62372010cf1a8103d5f61874f2582866
akorede <emiolasaheed1@gmail.com>
2023-10-24 10:19:36 +0100

    Fix linters errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   has_many :likes
   has_many :posts, foreign_key: 'author_id'
   validates :name, presence: true
-  validates :posts_counter, numericality: { only_integer: true}
-  validates :posts_counter, comparison: { greater_than_or_equal_to: 0}
+  validates :posts_counter, numericality: { only_integer: true }
+  validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
 
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   let(:user) { User.new(name: 'Jerry', photo: 'jerry.png', bio: 'This is Jerry bio', posts_counter: 0) }
-  subject { Post.new(author: user, title: 'My first post', text: 'Content of my post', comments_counter: 0, likes_counter: 0) }
+  subject do
+    Post.new(author: user, title: 'My first post', text: 'Content of my post', comments_counter: 0, likes_counter: 0)
+  end
 
   before { subject.save }
   it 'is valid with the required attributes' do
