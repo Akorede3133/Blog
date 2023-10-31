cbc8be3ba2a02d957a1ead237aba93dd9f7d11f0
akorede <emiolasaheed1@gmail.com>
2023-10-31 19:36:42 +0100

    Style like button

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,4 +7,10 @@ class LikesController < ApplicationController
       redirect_to user_post_path(current_user, @post)
     end
   end
+
+  def destroy
+    @like = Like.find(params[:id])
+    @like.destroy
+    redirect_to user_post_path(current_user, @like.post)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def slice_text(text)
     text.length > 50 ? "#{text[0, 50]}..." : text
   end
+
+  def user_already_liked?(post, user)
+    user_id = user.id
+    like = post.likes.find_by(user: user_id)
+    like ? true : false
+  end
+  def find_like(post, user)
+    user_id = user.id
+    like = post.likes.find_by(user: user_id)
+  end
 end
