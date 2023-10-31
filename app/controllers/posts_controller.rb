bff995500171c2eef621bbba616bdeff34aecc89
akorede <emiolasaheed1@gmail.com>
2023-10-31 14:52:40 +0100

    Create post for current user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,21 @@ class PostsController < ApplicationController
   def new
     @post = current_user.posts.build
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "Post created successfully!"
+      redirect_to user_post_path(current_user, @post)
+    else
+      flash.now[:error] = "Post was not created!"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
