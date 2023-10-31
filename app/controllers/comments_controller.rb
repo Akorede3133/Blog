6b513bb4df76dc64df38d81fac3ca206c6629bba
akorede <emiolasaheed1@gmail.com>
2023-10-31 17:00:15 +0100

    Create commit for current user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,17 @@
 class CommentsController < ApplicationController
-  
+  def create
+    @post = current_user.posts.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to user_post_path(current_user, @post)
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: :create
+    end
   end
-
-  resources :comments, only: :create
 end
