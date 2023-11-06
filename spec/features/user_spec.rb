136590a4e561354b9fe6b07fb410819e5d7055ed
agneta1991 <pupienytea@gmail.com>
2023-11-06 13:46:25 +0200

    Added tests for user show page

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -34,4 +34,45 @@ RSpec.describe 'User', type: :feature do
       end
     end
   end
+
+  context "show page" do
+    it 'shows profile picture' do
+      user = User.first
+      visit user_path(user)
+        expect(page).to have_selector("img[src='#{user.photo}']")
+    end
+
+    it "show user name" do
+      user = User.first
+      visit user_path(user)
+        expect(page).to have_content(user.name)
+    end
+
+    it "show number of posts by the user" do
+      user = User.first
+      visit user_path(user)
+      expect(page).to have_content("Number of posts: #{user.posts_counter}")
+    end
+
+    it "show user bio" do
+      user = User.first
+      visit user_path(user)
+      expect(page).to have_content("#{user.bio}")
+    end
+
+    it "show user's first three posts" do
+      user = User.first
+      visit user_path(user)
+      if user.posts_counter == 0
+      expect(page).to have_content('This user has no post at the moment')
+      elsif user.posts_counter > 0 && < 3 
+        user.posts.each do |post|
+          expect(page).to have_content(post.title)
+          expect(page).to have_content(post.text)
+        end
+      else 
+        expect(page).to have_selector('a', text:'See all posts')
+      end
+    end
+  end
 end
