5f6f26237b35e02c12814383015b63cf90deaf11
akorede <emiolasaheed1@gmail.com>
2023-11-07 11:27:02 +0100

    Fix test errors

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Post', type: :feature do
-  let(:user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
-  let(:post) { Post.create(author: user, title: 'Hello', text: 'This is my first post') }
+  let(:user) { User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  let(:post) { Post.new(author: user, title: 'Hello', text: 'This is my first post') }
 
-
+  before { user.save }
+  
   context 'index page' do
     it "show user's username" do
       visit user_posts_path(user)
