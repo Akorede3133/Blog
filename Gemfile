250ad984fb24398ac0aa401a7e368b5f0cf77d53
akorede <emiolasaheed1@gmail.com>
2023-11-07 21:25:23 +0100

    Set up devise gem

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '3.2.2'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.1.1'
+gem "devise"
 
 gem 'rubocop', '>= 1.0', '< 2.0'
 
@@ -55,6 +56,7 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem 'web-console'
+  gem "letter_opener"
 
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true }
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }
+  validates :username, presence: true, uniqueness: true
+
 
   def most_recent_posts
     posts.order(created_at: :desc).limit(3)
